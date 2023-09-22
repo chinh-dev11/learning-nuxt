ac0abfc0cf00dfb11280270f0d0f4caa3931527b
Chinh Le <49584276+chinh-dev11@users.noreply.github.com>
2023-09-22 15:03:24 -0400

    Add data fetching asyncData using nuxt/http package

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,25 @@
 <template>
   <!-- <Tutorial /> -->
-  <div><h1>Index Page</h1></div>
+  <div>
+    <h1>Rivers</h1>
+    <ul>
+      <li v-for="(river, index) in rivers" :key="`river-${index}`">
+        {{ river.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async asyncData({ $http }) {
+    const apiUrl = 'https://api.nuxtjs.dev/rivers'
+    const resHttp = await $http.$get(apiUrl)
+    // console.log('$http - resHttp', resHttp)
+    return {
+      rivers: resHttp
+    }
+  }
 }
 </script>
